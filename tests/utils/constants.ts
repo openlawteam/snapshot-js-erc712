2df1c558bb138e55e32866978af7c722bd274a77
Jeremiah Naylor-Trein <jeremiahtrein@gmail.com>
2021-06-01 17:11:00 +0100

    adds jest test suite

---
+++ tests/utils/constants.ts
@@ -0,0 +1,10 @@
+export const DEFAULT_ETH_ADDRESS: string =
+  "0xE11BA2b4D45Eaed5996Cd0823791E0C93114882d";
+
+export const DEFAULT_SIG: string =
+  "0x767aab47ea6b8c204a773c1b6e1818b985802442cbed4c8bbc3b88962fff138e38e3c86635a36548feed3045a11ef4b25003074c750b1a67b9bb5b2b5997ca971b";
+
+export const DEFAULT_PROPOSAL_ID: string =
+  "0xf96e930cc62ff54c1bd131db67fdb78238611fbf47eef007f136d96c4f309873";
+
+export const DEFAULT_TIMESTAMP: number = Math.floor(Date.now() / 1000);
